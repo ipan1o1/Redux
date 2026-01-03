0d2ad265072c2397e385e68a2d4524fb42d22b96
ipan1o1 <ipanmonsengo@gmail.com>
2026-01-03 18:36:46 +0100

    lol

--- Ball.java
+++ Ball.java
@@ -3,7 +3,7 @@ public class Ball extends Entity{
     // Attributes
     private double x, y;
     private double vx, vy;
-    private double radius = 0.3;
+    private double radius = 0.3; //some bs
 
     // Methodes 
        
